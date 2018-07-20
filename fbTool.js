cef9245e9c4bf929663ba1cc6b8cad72fa1fd531
Half-Straw <j80304xz@gmail.com>
2018-07-20 18:19:01 +0800

    修正Bug_bodyID.appendChild(newDiv);

--- fbTool.js
+++ fbTool.js
@@ -11,7 +11,7 @@ function openFb(fbLink) {
     var newDiv = document.createElement('div');
     newDiv.id = 'cus_ser';
     newDiv.innerHTML = '<div class="cus_ser_"><div class="title"></div><ul><li><iframe src="https://www.facebook.com/plugins/likebox.php?href=https://www.facebook.com/' + fbLink + '/&width=270&colorscheme=light&show_faces=true&stream=true&header=true&height=577" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:270px; height:577px;position: absolute;top: 0px;"allowTransparency="true"></iframe><div style="clear: both;"></div></li></ul><div id="copyright"><a href="https://www.half-straw.com/">小工具製作：半節吸管HalfStraw</a></div></div>';
-    bodyID.appendChild(newDiv)
+    bodyID.appendChild(newDiv);
 
     var fbOpen = false;
     document.getElementById("cus_ser").onclick = function() {
