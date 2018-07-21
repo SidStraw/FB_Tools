df8ca5ae582330ef17b2e1064dc68b0c611838d8
Half-Straw <j80304xz@gmail.com>
2018-07-21 12:32:43 +0800

    debug: .title rename .fbTitle

--- fbTool.js
+++ fbTool.js
@@ -27,7 +27,7 @@ function openFb(fbLink) {
     var bodyID = document.getElementsByTagName("body")[0];
     var newDiv = document.createElement('div');
     newDiv.id = 'cus_ser';
-    newDiv.innerHTML = '<div class="cus_ser_"><div class="title"></div><ul><li><iframe src="https://www.facebook.com/plugins/likebox.php?href=https://www.facebook.com/' + fbLink + '/&width=270&colorscheme=light&show_faces=true&stream=true&header=true&height=577" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:270px; height:577px;position: absolute;top: 0px;"allowTransparency="true"></iframe><div style="clear: both;"></div></li></ul><div id="copyright"><a href="https://www.half-straw.com/">小工具製作：半節吸管HalfStraw</a></div></div>';
+    newDiv.innerHTML = '<div class="cus_ser_"><div class="fbTitle"></div><ul><li><iframe src="https://www.facebook.com/plugins/likebox.php?href=https://www.facebook.com/' + fbLink + '/&width=270&colorscheme=light&show_faces=true&stream=true&header=true&height=577" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:270px; height:577px;position: absolute;top: 0px;"allowTransparency="true"></iframe><div style="clear: both;"></div></li></ul><div id="copyright"><a href="https://www.half-straw.com/">小工具製作：半節吸管HalfStraw</a></div></div>';
     bodyID.appendChild(newDiv);
 
     var fbOpen = false;
